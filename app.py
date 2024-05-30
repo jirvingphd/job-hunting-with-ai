ca343da0367c3f573f12d94b5f981489757ad0a6
James M. Irving, Ph.D. <james.irving.phd@gmail.com>
2024-05-30 14:53:11 -0400

    Update app.py

--- app.py
+++ app.py
@@ -63,6 +63,8 @@ with c2:
 
         
 md_instructions = """
+>- 📺 Video Walkthrough: https://youtu.be/CZrarP5_6Rw
+
 - 👈 **First, open sidebar (`>`) to add your 🔑 OpenAI API Key** and select which ChatGPT model (Default is gpt-4o).
 - 👇**Next, open the menu below to 📄Upload Resume and Job Listing**
     - Make sure to press the  `Update Resume` or `Update Job Listing` buttons after uploading or pasting the text.
@@ -71,7 +73,6 @@ md_instructions = """
 
 - (Optional) 📥*Download the chat history at the end of the session.*
 
-- 📺 Video Walkthrough: https://youtu.be/CZrarP5_6Rw
 
 """
 with st.expander("Instructions", expanded=True):
